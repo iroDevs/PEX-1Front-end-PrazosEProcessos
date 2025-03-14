de5befb520c4c4ac351cea971bb526e16260a3b2
PedroDev <pedrohenriquelouresoliveira@gmail.com>
2025-03-13 23:01:11 -0300

    estrutura inicial react vite

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <>
+      <h1>pedro</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+
+import App from './App.tsx'
+
+//css
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Global.css'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
